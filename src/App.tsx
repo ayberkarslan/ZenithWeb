d8a5716ccf0881d21e8cb9e7a45bbdd1a3ab7c1f
ayberkarslan <ayberkarslan@users.noreply.github.com>
2026-06-28 13:36:00 +0300

    fix nested router error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -7,19 +7,17 @@ import Team from './pages/Team'
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/dev-log" element={<DevLog />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app-container">
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/dev-log" element={<DevLog />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
